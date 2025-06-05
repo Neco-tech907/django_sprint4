3dd810806a76a4b48b0f3a77d9ce02e7a800bb1c
Raul <ivanovraul555@gmail.com>
2025-06-05 06:25:31 +0300

    final

---
+++ blogicum/blog/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from django.utils import timezone
+from django.contrib.auth import get_user_model
+from .models import Post, Comment
+
+User = get_user_model()
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Post
+        # Указываем, что надо отобразить все поля.
+        fields = ['title', 'text', 'pub_date', 'location', 'category',
+                  'is_published', 'image']
+        widgets = {
+            'pub_date': forms.DateTimeInput(attrs={
+                'type': 'datetime-local',  # HTML5 input type
+                'class': 'form-control'   # Дополнительные классы, если нужно
+            })
+        }
+
+    def clean_publiclic_date(self):
+        pub_date = self.cleaned_data['pub_date']
+        if pub_date:
+            # Ensure pub_date is timezone-aware
+            if not timezone.is_aware(pub_date):
+                # Assume input is in the project's TIME_ZONE and make it aware
+                pub_date = timezone.make_aware(
+                    pub_date, timezone=timezone.get_default_timezone())
+            return pub_date
+        return pub_date
+
+
+class CommentariesForm(forms.ModelForm):
+    class Meta:
+        # Указываем модель, на основе которой должна строиться форма.
+        model = Comment
+        # Указываем, что надо отобразить все поля.
+        fields = ['text']
